f3fd1518633dc950fe086f29f6b2cd9039c8373f
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-12 15:46:24 +0600

    `Removed unused imports and code, updated main.jsx to use Routes from routes.jsx`

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { createBrowserRouter, RouterProvider } from "react-router";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+import { RouterProvider } from "react-router/dom";
+import { router } from "./Routes/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
